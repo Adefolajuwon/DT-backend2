2acfdd998bc43cb7c1bce65e31f606c4652b65cc
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-06-17 13:38:36 +0100

    change

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
-// app.use('/api', routes);
+
+app.use(express.json());
+app.use('/api/v3/app', routes);
 
 module.exports = app;
